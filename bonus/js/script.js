fb2679ae3dd46bfae8e0ed865aeb0ce0630d5098
MonicaAuditore <mauditore@live.it>
2023-01-12 16:28:12 +0100

    init_inizio bonus

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,4 +1,18 @@
 /*
+I PARTE
+
+1. creo le celle con classe cella all'interno della griglia;
+2. al click del bottone "play" rendo visibile la griglia;
+3. assegno un numero alle celle;
+4. coloro di azzurro la casella cliccata;
+5. stampo in console il numero che è stato cliccato.
+
+6. aggiungo la select in html;
+7. in css creo altre due classi per cella-medium e cella-hard;
+8. creo le altre due griglie;
+9. al click, in base alla difficoltà scelta, mostro una griglia diversa;
+
+----------------------------------------------------------------------------------------------------
 
 II PARTE
 
@@ -16,6 +30,12 @@ La partita termina quando il giocatore clicca su una bomba o quando raggiunge il
 (ovvero quando ha rivelato tutte le celle che non sono bombe).
 Al termine della partita il software deve comunicare il punteggio, 
 cioè il numero di volte che l'utente ha cliccato su una cella che non era una bomba.
+
+BONUS:
+Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
+- difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+- difficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+- difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
 10. genero 16 numeri casuali da 1 a 100;
 11. li inserisco in un array, se nell'array in numero è già presente ne genero uno nuovo altrimenti lo inserisco;
@@ -100,9 +120,73 @@ for (let i = 1; i <= 100; i++) {
   });
 }
 
+console.log(
+  "-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+);
+
+// creo GRIGLIA 2;
+// creo le celle con classe cella all'interno della griglia;
+for (let i = 1; i <= 81; i++) {
+  // console.log(i);
+  let div = document.createElement("div");
+
+  // assegno un numero alle celle;
+  div.innerHTML += i;
+  div.classList.add("cella-medium");
+  grigliaDue.append(div);
+
+  // coloro di azzurro la casella cliccata;
+  div.addEventListener("click", function () {
+    if (this.classList.contains("cellaCliccata")) {
+      this.classList.remove("cellaCliccata");
+    } else {
+      this.classList.add("cellaCliccata");
+    }
+
+    // stampo in console il numero che è stato cliccato.
+    console.log("Cella cliccata numero:", i);
+  });
+}
+
+// creo GRIGLIA 3;
+// creo le celle con classe cella all'interno della griglia;
+for (let i = 1; i <= 49; i++) {
+  // console.log(i);
+  let div = document.createElement("div");
+
+  // assegno un numero alle celle;
+  div.innerHTML += i;
+  div.classList.add("cella-hard");
+  grigliaTre.append(div);
+
+  // coloro di azzurro la casella cliccata;
+  div.addEventListener("click", function () {
+    if (this.classList.contains("cellaCliccata")) {
+      this.classList.remove("cellaCliccata");
+    } else {
+      this.classList.add("cellaCliccata");
+    }
+
+    // stampo in console il numero che è stato cliccato.
+    console.log("Cella cliccata numero:", i);
+  });
+}
+
 const livelli = document.getElementById("levels");
 
 // al click del bottone "play" mostro la griglia in base alla scelta della difficoltà;
 bottone.addEventListener("click", function () {
-  grigliaUno.classList.remove("hidden");
+  if (livelli.value == "e") {
+    grigliaUno.classList.remove("hidden");
+    grigliaDue.classList.add("hidden");
+    grigliaTre.classList.add("hidden");
+  } else if (livelli.value == "m") {
+    grigliaDue.classList.remove("hidden");
+    grigliaUno.classList.add("hidden");
+    grigliaTre.classList.add("hidden");
+  } else if (livelli.value == "h") {
+    grigliaTre.classList.remove("hidden");
+    grigliaUno.classList.add("hidden");
+    grigliaDue.classList.add("hidden");
+  }
 });
